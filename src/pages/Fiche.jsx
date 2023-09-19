74305967c2f9a9d6f032b2c20c55d2d546a7bea9
Ailyza <131762373+Ailyza@users.noreply.github.com>
2023-09-19 23:13:25 +0200

    habitation

--- src/pages/Fiche.jsx
+++ src/pages/Fiche.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 const Fiche = () => {
-  return (
+  const location = useLocation();
+  const logement = location.state?.element;
+  console.log(logement)
+   return (
     <div>
+
       <p>Fiche</p>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import Banner from '../components/Banner';
+import axios from 'axios';
+import Habitations from '../components/Habitations';
+
 
 const Home = () => {
+  const [data,setData] = useState([])
+  useEffect(() => {
+    axios.get("logement.json")
+    .then((res)=> setData(res.data))
+    .catch((error)=> console.log(error))
+  },[])
   return (
     <div>
       <Banner/>
+      <Habitations element={data}/>
     </div>
+    
   
   );
 
